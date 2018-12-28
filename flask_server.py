0cc9095c47f72f1b8c19676ada8953fc568849ce
Pranav Raghavan <gtalkpranav@gmail.com>
2018-12-28 12:12:00 +0530

    working with edit and delete and flasking messages

--- flask_server.py
+++ flask_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -6,13 +6,14 @@ from database_setup import Base, Restaurant, MenuItem
 app = Flask(__name__)
 
 engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 
 @app.route('/')
 def defaultRestaurantMenu():
-    Base.metadata.bind = engine
-
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
 
     restaurant = session.query(Restaurant).first()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
@@ -21,9 +22,9 @@ def defaultRestaurantMenu():
 
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
-    Base.metadata.bind = engine
-
-    DBSession = sessionmaker(bind=engine)
+    # Base.metadata.bind = engine
+    #
+    # DBSession = sessionmaker(bind=engine)
     session = DBSession()
 
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).first()
@@ -32,10 +33,10 @@ def restaurantMenu(restaurant_id):
 
 @app.route('/restaurants/<int:restaurant_id>/new', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    Base.metadata.bind = engine
-
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
+    # Base.metadata.bind = engine
+    #
+    # DBSession = sessionmaker(bind=engine)
+    # session = DBSession()
 
     if request.method == 'POST':
         newItem = MenuItem(name=request.form['name'], description=request.form[
@@ -50,9 +51,9 @@ def newMenuItem(restaurant_id):
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit',
            methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    Base.metadata.bind = engine
-
-    DBSession = sessionmaker(bind=engine)
+    # Base.metadata.bind = engine
+    #
+    # DBSession = sessionmaker(bind=engine)
     session = DBSession()
 
     # print(menu_id)
@@ -63,6 +64,7 @@ def editMenuItem(restaurant_id, menu_id):
             edited.name = request.form['name']
         session.add(edited)
         session.commit()
+        flash("Edited menu item")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         # USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU
@@ -74,9 +76,9 @@ def editMenuItem(restaurant_id, menu_id):
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/',
            methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-    Base.metadata.bind = engine
-
-    DBSession = sessionmaker(bind=engine)
+    # Base.metadata.bind = engine
+    #
+    # DBSession = sessionmaker(bind=engine)
     session = DBSession()
 
     edited = session.query(MenuItem).filter_by(id=menu_id).one()
@@ -84,11 +86,13 @@ def deleteMenuItem(restaurant_id, menu_id):
         if edited:
             session.delete(edited)
             session.commit()
+            flash("Deleted menu item")
             return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
 
     else:
         return render_template("deletemenuitem.html",  restaurant_id=restaurant_id, menu_id=menu_id, item=edited)
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)

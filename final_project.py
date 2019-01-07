2e1c861c44a4cc5d06cfab13ed50799b9a0dee6a
Pranav Raghavan <gtalkpranav@gmail.com>
2019-01-07 14:13:46 -0800

    added all the routes needed

--- final_project.py
+++ final_project.py
@@ -11,29 +11,45 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def allRestaurants():
+@app.route('/restaurants/')
+def showRestaurants():
     return "This is all the restaurants"
 
 
-@app.route('/restaurants/create')
-def createRestaurant():
-    return "Create a restaurant page"
+@app.route('/restaurant/new/')
+def newRestaurant():
+    return "Create a new restaurant"
 
 
-@app.route('/restaurants/edit/<int:restaurant_id>/')
+@app.route('/restaurant/edit/<int:restaurant_id>/')
 def editRestaurant(restaurant_id):
     return "Edit a restaurant {}".format(restaurant_id)
 
 
-@app.route('/restaurants/delete/<int:restaurant_id>/')
+@app.route('/restaurant/delete/<int:restaurant_id>/')
 def deleteRestaurant(restaurant_id):
     return "Delete a restaurant {}".format(restaurant_id)
 
 
-@app.route('/restaurants/new/')
-def newRestaurant():
-    return "Create a new restaurant"
+@app.route('/restaurant/<int:restaurant_id>/')
+@app.route('/restaurant/<int:restaurant_id>/menu')
+def showMenu(restaurant_id):
+    return "showing restaurant menu {}".format(restaurant_id)
 
+
+@app.route('/restaurant/<int:restaurant_id>/menu/new')
+def newMenuItem(restaurant_id):
+    return "New manu item for restaurant {}".format(restaurant_id)
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+    return "edit manu item for restaurant {}, menu_id {}".format(restaurant_id, menu_id)
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "delete manu item for restaurant {}, menu item {}".format(restaurant_id, menu_id)
 
 
 if __name__ == '__main__':
